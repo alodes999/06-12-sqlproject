047369426543d53b96ac12d88a2bf7422062b5e4
Andrew Lodes <alodes999@gmail.com>
2015-06-14 16:23:24 -0500

    Add documentation to app

--- app.rb
+++ app.rb
@@ -13,10 +13,16 @@ EIM.execute("CREATE TABLE IF NOT EXISTS ship_locations (id INTEGER PRIMARY KEY, 
 EIM.results_as_hash = true
 
 # `````````````````````````````````````````````````````````````````````````````````````````````````````
+# Here I set 3 different variables with ranges to check later in our while loops
+#
+# These are here because I tried to have an actual Lists Module, but I couldn't
+# get the logic working between the module and if/else statements, so I went
+# less DRY and just put the actual menus and logic here in app, instead of a
+# separate Lists Module.
 bad_choice_threelist = (4..8)
 bad_choice_fourlist = (5..8)
 bad_choice_fivelist = (6..8)
-
+# Our main menu, with options and logic to direct those options
 puts "Hello! What would you like to do today?"
 puts "1 - Look at a list"
 puts "2 - Enter a new record"
@@ -25,26 +31,38 @@ puts "4 - Delete a record"
 puts "9 - Quit"
 puts "Please enter a menu option:"
 choice = gets.chomp.to_i
+# We have the entire menu structure in a while loop, letting it cycle until we get a Quit command with 9
 while choice != 9
+  # This part of the loop ensures we have a valid option, and will reprompt for a choice.  If 9 is desired, it will reprompt the main
+  # menu again.
   while bad_choice_fourlist.include?(choice) || choice > 9 || choice == 0 
     puts "That is not a valid option, please reenter an option"
     choice = gets.chomp.to_i
   end
-
+  # This case loop will relay the choice from our menu into where we want to go.
+  # 1 takes us to our lists
+  # 2 takes us to our new entry menu
+  # 3 takes us to our edit menu
+  # 4 takes us to our delete menu 
   case choice
+  # Here is our Lists sub-menu, accessed when the user picks 1 from the main menu
+  # 
+  # We have 5 options, and a further case loop to execute the options for each list option
   when 1
     puts "Which list would you like to look at?"
     puts "1 - Look at ships list"
     puts "2 - Look at ship types list"
     puts "3 - Look at ship locations list"
+    puts "4 - Look at all ships in a given type"
+    puts "5 - Look at all ships in a given location"
     puts "9 - Go back"
-    
+    # This is our logic to get a valid choice again
     listchoice = gets.chomp.to_i
-    while bad_choice_threelist.include?(listchoice) || listchoice > 9 || listchoice == 0
+    while bad_choice_fivelist.include?(listchoice) || listchoice > 9 || listchoice == 0
       puts "That is not a valid option, please reenter an option"
       listchoice = gets.chomp.to_i
     end
-    
+    # Our case loop for the submenu for lists.
     case listchoice
     when 1
       puts ShipName.all
@@ -52,23 +70,27 @@ while choice != 9
       puts ShipType.all
     when 3
       puts ShipLocation.all
+    when 4
+      
+    when 5
+      
     when 9
       puts "Ok, back to the top!"
     end
-    
+  # Our Entry sub-menu.  Here we define new rows for our tables and insert them  
   when 2
     puts "What entry would you like to make?"
     puts "1 - Enter a new ship type"
     puts "2 - Enter a new ship location"
     puts "3 - Enter a new ship"
     puts "9 - Go back"
-    
+    # Our valid choice logic
     listchoice = gets.chomp.to_i
     while bad_choice_threelist.include?(listchoice) || listchoice > 9 || listchoice == 0
       puts "That is not a valid option, please reenter an option"
       listchoice = gets.chomp.to_i
     end
-    
+    # Our case list for our Entry sub-menu
     case listchoice
     when 1
       puts "Ok, what ship type would you like to add?"
@@ -102,20 +124,23 @@ while choice != 9
     when 9
       puts "Ok, back to the top!"
     end
-  
+  # Here is our Editing sub-menu.  Here we define changes to our list items and send those
+  # into our lists
   when 3
     puts "What would you like to change?"
     puts "1 - Change a ship record"
     puts "2 - Change a ship type record"
     puts "3 - Change a location record"
     puts "9 - Go back"
-    
+    # Again, our choice logic
     listchoice = gets.chomp.to_i
     while bad_choice_threelist.include?(listchoice) || listchoice > 9 || listchoice == 0
       puts "That is not a valid option, please reenter an option"
       listchoice = gets.chomp.to_i
     end
-    
+    # Here is our Edit Ships sub-menu.  We define the ship changes here.
+    # I elected to put the ship changes in a separate sub-menu
+    # because there are multiple values that can change.
     case listchoice
     when 1
       puts "Ok, how would you like to modify a ship entry?"
@@ -124,13 +149,13 @@ while choice != 9
       puts "3 - Change a ship type"
       puts "4 - Change a ship location"
       puts "9 - Go back"
-      
+      # More choice logic
       listchoice = gets.chomp.to_i
       while bad_choice_fourlist.include?(listchoice) || listchoice > 9 || listchoice == 0      
         puts "That is not a valid option, please reenter an option"
         listchoice = gets.chomp.to_i
       end
-      
+      #Our case logic for this Edit Ships sub-menu
       case listchoice
       when 1
         puts "What's the id of the ship you want to change?"
@@ -200,20 +225,25 @@ while choice != 9
     when 9
       puts "Ok, back to the top!"
     end
-  
+  # Our Delete sub-menu.  We define which items to delete, and verify the user wishes to delete
+  # 
+  # The type and location options have additional logic preventing deletion
+  # in the event there is a type or location currently attached to a ship.
+  # In this case, the user is prompted with a message saying there is something
+  # attached and to remove that link before deleting the type/location.
   when 4
     puts "What would you like to delete?"
     puts "1 - Delete a ship record"
     puts "2 - Delete a ship type"
     puts "3 - Delete a ship location"
     puts "9 - Go back"
-    
+    # More tasty choice logic
     listchoice = gets.chomp.to_i
     while bad_choice_threelist.include?(listchoice) || listchoice > 9 || listchoice == 0
       puts "That is not a valid option, please reenter an option"
       listchoice = gets.chomp.to_i
     end
-    
+    # Our case loop for the Delete sub-menu
     case listchoice
     when 1
       puts "Ok, which ship would you like to delete? Please enter the id of the ship:"
@@ -267,6 +297,8 @@ while choice != 9
     when 9
       puts "Ok, back to the top!"
     end
+  when 9
+    puts "To the Main Menu"
   end
   puts "Main Menu"
   puts "1 - Look at a list"
