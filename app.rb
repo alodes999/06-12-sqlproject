e7b6d1143b3fe08c5da7cd746e77005d9a92b742
Andrew Lodes <alodes999@gmail.com>
2015-06-15 11:54:40 -0500

    Recommit add of refactored app with lists support

--- app.rb
+++ app.rb
@@ -14,29 +14,18 @@ EIM.execute("CREATE TABLE IF NOT EXISTS ship_locations (id INTEGER PRIMARY KEY, 
 EIM.results_as_hash = true
 
 # `````````````````````````````````````````````````````````````````````````````````````````````````````
-# Here I set 3 different variables with ranges to check later in our while loops
-#
-# These are here because I tried to have an actual Lists Module, but I couldn't
-# get the logic working between the module and if/else statements, so I went
-# less DRY and just put the actual menus and logic here in app, instead of a
-# separate Lists Module.
-bad_choice_threelist = (4..8)
-bad_choice_fourlist = (5..8)
-bad_choice_fivelist = (6..8)
+# Here I set a variable with ranges to check later in our main while loop
+bad_choice = (5..8)
 # Our main menu, with options and logic to direct those options
 puts "Hello! What would you like to do today?"
-puts "1 - Look at a list"
-puts "2 - Enter a new record"
-puts "3 - Change a record"
-puts "4 - Delete a record"
-puts "9 - Quit"
+ShipLists.main_menu
 puts "Please enter a menu option:"
 choice = gets.chomp.to_i
 # We have the entire menu structure in a while loop, letting it cycle until we get a Quit command with 9
 while choice != 9
   # This part of the loop ensures we have a valid option, and will reprompt for a choice.  If 9 is desired, it will reprompt the main
   # menu again.
-  while bad_choice_fourlist.include?(choice) || choice > 9 || choice == 0 
+  while bad_choice.include?(choice) || choice > 9 || choice == 0 
     puts "That is not a valid option, please reenter an option"
     choice = gets.chomp.to_i
   end
@@ -51,18 +40,8 @@ while choice != 9
   # We have 5 options, and a further case loop to execute the options for each list option
   when 1
     puts "Which list would you like to look at?"
-    puts "1 - Look at ships list"
-    puts "2 - Look at ship types list"
-    puts "3 - Look at ship locations list"
-    puts "4 - Look at all ships in a given type"
-    puts "5 - Look at all ships in a given location"
-    puts "9 - Go back"
-    # This is our logic to get a valid choice again
-    listchoice = gets.chomp.to_i
-    while bad_choice_fivelist.include?(listchoice) || listchoice > 9 || listchoice == 0
-      puts "That is not a valid option, please reenter an option"
-      listchoice = gets.chomp.to_i
-    end
+    ShipLists.option_one
+    listchoice = ShipLists.list_five_choice
     # Our case loop for the submenu for lists.
     case listchoice
     when 1
@@ -87,16 +66,8 @@ while choice != 9
   # Our Entry sub-menu.  Here we define new rows for our tables and insert them  
   when 2
     puts "What entry would you like to make?"
-    puts "1 - Enter a new ship type"
-    puts "2 - Enter a new ship location"
-    puts "3 - Enter a new ship"
-    puts "9 - Go back"
-    # Our valid choice logic
-    listchoice = gets.chomp.to_i
-    while bad_choice_threelist.include?(listchoice) || listchoice > 9 || listchoice == 0
-      puts "That is not a valid option, please reenter an option"
-      listchoice = gets.chomp.to_i
-    end
+    ShipLists.option_two
+    listchoice = ShipLists.list_three_choice
     # Our case list for our Entry sub-menu
     case listchoice
     when 1
@@ -135,33 +106,16 @@ while choice != 9
   # into our lists
   when 3
     puts "What would you like to change?"
-    puts "1 - Change a ship record"
-    puts "2 - Change a ship type record"
-    puts "3 - Change a location record"
-    puts "9 - Go back"
-    # Again, our choice logic
-    listchoice = gets.chomp.to_i
-    while bad_choice_threelist.include?(listchoice) || listchoice > 9 || listchoice == 0
-      puts "That is not a valid option, please reenter an option"
-      listchoice = gets.chomp.to_i
-    end
+    ShipLists.option_three
+    listchoice = ShipLists.list_three_choice
     # Here is our Edit Ships sub-menu.  We define the ship changes here.
     # I elected to put the ship changes in a separate sub-menu
     # because there are multiple values that can change.
     case listchoice
     when 1
       puts "Ok, how would you like to modify a ship entry?"
-      puts "1 - Change a ship name"
-      puts "2 - Change a ship cost"
-      puts "3 - Change a ship type"
-      puts "4 - Change a ship location"
-      puts "9 - Go back"
-      # More choice logic
-      listchoice = gets.chomp.to_i
-      while bad_choice_fourlist.include?(listchoice) || listchoice > 9 || listchoice == 0      
-        puts "That is not a valid option, please reenter an option"
-        listchoice = gets.chomp.to_i
-      end
+      ShipLists.option_three_one
+      listchoice = ShipLists.list_four_choice
       #Our case logic for this Edit Ships sub-menu
       case listchoice
       when 1
@@ -239,18 +193,9 @@ while choice != 9
   # In this case, the user is prompted with a message saying there is something
   # attached and to remove that link before deleting the type/location.
   when 4
-    puts "What would you like to delete?"
-    puts "1 - Delete a ship record"
-    puts "2 - Delete a ship type"
-    puts "3 - Delete a ship location"
-    puts "9 - Go back"
-    # More tasty choice logic
-    listchoice = gets.chomp.to_i
-    while bad_choice_threelist.include?(listchoice) || listchoice > 9 || listchoice == 0
-      puts "That is not a valid option, please reenter an option"
-      listchoice = gets.chomp.to_i
-    end
-    # Our case loop for the Delete sub-menu
+    ShipLists.option_four
+    listchoice = ShipLists.list_three_choice
+    
     case listchoice
     when 1
       puts "Ok, which ship would you like to delete? Please enter the id of the ship:"
@@ -308,11 +253,7 @@ while choice != 9
     puts "To the Main Menu"
   end
   puts "Main Menu"
-  puts "1 - Look at a list"
-  puts "2 - Enter a new record"
-  puts "3 - Change a record"
-  puts "4 - Delete a record"
-  puts "9 - Quit"
+  ShipLists.main_menu
   
   puts "What would you like to do next?"
   choice = gets.chomp.to_i
