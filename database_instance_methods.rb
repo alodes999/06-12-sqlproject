854a7e62a270c4a3564be4f91203aad2ce0e1f97
Andrew Lodes <alodes999@gmail.com>
2015-06-18 14:28:51 -0500

    ORM Refactoring

--- database_instance_methods.rb
+++ database_instance_methods.rb
@@ -2,10 +2,20 @@ require 'active_support'
 require 'active_support/inflector'
 
 module DatabaseInstanceMethods
+  # Returns the stored value in the field passed from our DB.  The id number the method looks
+  # for is referenced in the Object that the method is called on.
+  # 
+  # Accepts 1 argument, the field in our table we want to retrieve the information from
+  # 
+  # Returns a String or Integer, or whatever value is stored in the field we are requesting
   def get(field)
-    table_name = self.class.to_s.pluralize.underscore
+    table_name = self.class.to_s.tableize
     
     CONNECTION.execute("SELECT * FROM #{table_name} WHERE id = #{@id}}").first
     result[field]
+  end
+  
+  def save
+    
   end
 end
